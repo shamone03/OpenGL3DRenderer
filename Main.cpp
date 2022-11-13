311194cd743bf9dbdd97941e22f43bd4f529b39d
Aryah <ar341476@dal.ca>
2022-11-12 23:27:29 -0400

    draws a triangle now

--- Main.cpp
+++ Main.cpp
@@ -49,32 +49,48 @@ int main() {
 
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
-
-	// reference to array of references
-	// VBO = vertex buffer object
-	GLuint VAO, VBO;
-
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	//														static means vertices will be modified once and used many times
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
-	
-	
-	glClearColor(1, 187.f/255.f, 0, 1);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glfwSwapBuffers(window);
-
 	GLfloat vertices[] = {
 		-0.5f, -0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
 		0.0f, 0.5f, 0.0f
 	};
+	// reference to array of references
+	// VBO = vertex buffer object
+	GLuint VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//														static means vertices will be modified once and used many times
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//				pos of vert attrib / no of vals in each vert / type of vals / if coords are ints / space between each vert / offset, pointer to beginning of array
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	
+	
+	glfwSwapBuffers(window);
+
+	
 	
 	while (!glfwWindowShouldClose(window)) {
+		glClearColor(1, 187.f/255.f, 0, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		// use the data in this array
+		glBindVertexArray(VAO);
+		// specify primitive / start index of vert / number of verts
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+		glfwSwapBuffers(window);
 		glfwPollEvents(); // update window
 	}
-	
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
